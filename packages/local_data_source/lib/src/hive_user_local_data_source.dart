a8f10066585bc60a79208ac02dc0a721a601fbd4
amir1430 <amirhossein13791430@gmail.com>
2022-12-14 18:47:15 +0330

    refactor(local_data_source): improve performance and fix bugs

--- packages/local_data_source/lib/src/hive_user_local_data_source.dart
+++ packages/local_data_source/lib/src/hive_user_local_data_source.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:hive/hive.dart';
 import 'package:local_data_source/local_data_sorce.dart';
+import 'package:local_data_source/src/constants.dart';
 import 'package:collection/collection.dart';
 import 'package:rxdart/rxdart.dart';
 
@@ -10,151 +11,83 @@ class HiveUserLocalDataSource implements UserLocalDataSource {
     required Box<User> userBox,
   }) : _userBox = userBox {
     _usersController = BehaviorSubject.seeded(users);
-    _userController = BehaviorSubject();
-    _userDeletedController = BehaviorSubject();
-    _subscription = _userBox.watch().listen((event) {
-      print(event.value);
-      if (!event.deleted) {
-        _userController.add(event.value as User);
-      } else {
-        if (users.isEmpty) {
-          _usersController.add([]);
-        }
-        _userDeletedController.add(event.value as User);
-      }
-      _usersController.add(users);
+    _currentUserController = BehaviorSubject.seeded(currentUser);
+
+    _currentUserSubscription =
+        _userBox.watch(key: LocalDataSourceKeys.currentUser).listen((event) {
+      final _currentUser = currentUser;
+      _currentUserController.sink.add(_currentUser);
+      _usersController.sink.add(users);
     });
   }
 
   // controllers
   late final StreamController<List<User>> _usersController;
-  late final StreamController<User> _userController;
-  late final StreamController<User> _userDeletedController;
-  late final StreamSubscription<BoxEvent> _subscription;
+  late final StreamController<User?> _currentUserController;
 
-  // box
+  // stream subscriptions
+  late final StreamSubscription<BoxEvent> _currentUserSubscription;
+
+  // hive boxs
   final Box<User> _userBox;
 
   @override
-  Stream<User> get userStream => _userController.stream;
+  Stream<User?> get currentUserStream => _currentUserController.stream;
 
   @override
   Stream<List<User>> get usersStream => _usersController.stream;
 
   @override
-  List<User> get users => [..._userBox.values];
+  List<User> get users => _userBox
+      .toMap()
+      .entries
+      .map((e) => e.key != LocalDataSourceKeys.currentUser ? e.value : null)
+      .whereNotNull()
+      .toList();
 
   @override
-  User get currentUser =>
-      _userBox.values.firstWhereOrNull((element) => element.isCurrentUser)!;
+  User? get currentUser => _userBox.get(LocalDataSourceKeys.currentUser);
 
   @override
-  Future<void> alterUser(User user) async {
-    // final _getUser = _userBox.get(user.token, defaultValue: null);
-    // if (_getUser == null) {
-    //   print('_getUser != null');
-    //   await _userBox.put(user.token, user);
-    // }
-
-    final usersMap = <String, User>{
-      user.token: user..isCurrentUser = true,
-      for (final item in users)
-        if (item.token != user.token) item.token: item..isCurrentUser = false,
-    };
-    await _userBox.putAll(usersMap);
+  Future<void> add(User user) async {
+    await _userBox.putAll({
+      LocalDataSourceKeys.currentUser: user,
+      user.token: user,
+    });
   }
 
   @override
   Future<void> delete(User user) async {
-    try {
-      final otherUsers = [
-        for (final item in users)
-          if (item.token != user.token) item,
-      ];
+    await _userBox.delete(user.token);
 
-      await _userBox.delete(user.token);
-      if (otherUsers.isNotEmpty) {
-        final currentUser = otherUsers[0]..isCurrentUser = true;
-        await _userBox.put(currentUser.token, currentUser);
+    final _currentUser = currentUser;
+    if (_currentUser != null && _currentUser == user) {
+      final _newCurrent =
+          users.firstWhereOrNull((element) => element != _currentUser);
+      if (_newCurrent != null) {
+        await _userBox.put(LocalDataSourceKeys.currentUser, _newCurrent);
+      } else {
+        await _userBox.delete(LocalDataSourceKeys.currentUser);
       }
-    } catch (e) {
-      return;
     }
   }
 
   @override
-  Future<void> updateUser({
+  Future<void> update({
     required User oldUser,
     required User newUser,
   }) async {
-    if (oldUser == newUser) {
-      return;
+    await add(newUser);
+    if (oldUser != newUser) {
+      await delete(oldUser);
     }
-    if (oldUser.token != newUser.token) {
-      await _userBox.delete(oldUser.token);
-      return;
-    }
-    await _userBox.put(newUser.token, newUser);
   }
 
   @override
   Future<void> close() async {
-    await _subscription.cancel();
-    await _userController.close();
+    await _currentUserController.close();
     await _usersController.close();
-    await _userDeletedController.close();
+
+    await _currentUserSubscription.cancel();
   }
 }
-
-// @override
-// Future<List<User>> alterUser(User user) async {
-//   if (!user.isInBox) {
-//     await _userBox.add(user..isCurrentUser = true);
-//   }
-
-//   final usersMap = <dynamic, User>{
-//     for (var item in _userBox.values)
-//       if (item.key != user.key)
-//         item.key: item..isCurrentUser = false
-//       else
-//         user.key: user..isCurrentUser = true,
-//   };
-
-//   await _userBox.putAll(usersMap);
-//   return users;
-// }
-
-// @override
-// Future<List<User>> delete(User user) async {
-//   try {
-//     final otherUsers = [
-//       for (final item in users)
-//         if (item.key != user.key) item,
-//     ];
-
-//     if (otherUsers.isNotEmpty) {
-//       final currentUser = otherUsers[0]..isCurrentUser = true;
-//       await currentUser.save();
-//     }
-//     await user.delete();
-//     return users;
-//   } catch (e) {
-//     return users;
-//   }
-// }
-
-// @override
-// Future<List<User>> updateUser({
-//   required User oldUser,
-//   required User newUser,
-// }) async {
-//   if (oldUser == newUser) {
-//     return users;
-//   }
-//   if (oldUser.token != newUser.token) {
-//     await oldUser.delete();
-//   }
-//   await newUser.save();
-//   return users;
-// }
-
--- packages/local_data_source/lib/src/user_loca_data_source.dart
+++ packages/local_data_source/lib/src/user_loca_data_source.dart
@@ -3,19 +3,19 @@ import 'package:local_data_source/local_data_sorce.dart';
 abstract class UserLocalDataSource {
   const UserLocalDataSource();
 
-  Stream<User> get userStream;
+  Stream<User?> get currentUserStream;
 
   Stream<List<User>> get usersStream;
 
   List<User> get users;
 
-  User get currentUser;
+  User? get currentUser;
 
-  Future<void> alterUser(User user);
+  Future<void> add(User user);
 
   Future<void> delete(User user);
 
-  Future<void> updateUser({
+  Future<void> update({
     required User oldUser,
     required User newUser,
   });
