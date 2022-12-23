b10e6ae65f6ac911fc0361471e27ffd50ac4b0ff
amir1430 <amirhossein13791430@gmail.com>
2022-12-23 18:03:53 +0330

    fix(form_input): check server address only ends with number or letter

--- packages/form_input/lib/src/server_field.dart
+++ packages/form_input/lib/src/server_field.dart
@@ -12,7 +12,7 @@ class ServerField extends FormzInput<String, ServerFieldError> {
   const ServerField.pure([String value = '']) : super.pure(value);
 
   static final String _reg =
-      r"[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*).*[^/]$";
+      r"[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*).*[a-zA-Z0-9]$";
   // static bool _isServerValid(String value) {
   //   return Uri.parse(value).isAbsolute;
   // }
